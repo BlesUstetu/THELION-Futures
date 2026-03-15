7c74f0dc8568d4866e48db9cd98efda71c2a4275
BlesUstetu <lionustetu@gmail.com>
2026-03-15 09:13:19 +0100

    Update OpenOrders.jsx

--- src/tables/OpenOrders.jsx
+++ src/tables/OpenOrders.jsx
@@ -1,34 +1,39 @@
+import { useContext } from "react"
+import { TradingContext } from "../store/tradingStore"
+
 export default function OpenOrders(){
+
+const { openOrders } = useContext(TradingContext)
 
 return(
 
 <table>
 
 <thead>
-
 <tr>
-
 <th>Pair</th>
 <th>Side</th>
 <th>Price</th>
 <th>Amount</th>
 <th>Status</th>
-
 </tr>
-
 </thead>
 
 <tbody>
 
-<tr>
+{openOrders.map(o=>(
 
-<td>BTCUSDT</td>
-<td>LONG</td>
-<td>63900</td>
-<td>0.2</td>
-<td>OPEN</td>
+<tr key={o.id}>
+
+<td>{o.pair}</td>
+<td>{o.side}</td>
+<td>{o.price}</td>
+<td>{o.amount}</td>
+<td>{o.status}</td>
 
 </tr>
+
+))}
 
 </tbody>
 
