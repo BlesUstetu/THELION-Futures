79e37c929714b334d460ba36cae90340340e2bd7
BlesUstetu <lionustetu@gmail.com>
2026-03-15 07:00:46 +0100

    Create Positions.jsx

---
+++ src/tables/Positions.jsx
@@ -0,0 +1,41 @@
+export default function Positions(){
+
+return(
+
+<table>
+
+<thead>
+
+<tr>
+
+<th>Pair</th>
+<th>Size</th>
+<th>Entry</th>
+<th>Mark</th>
+<th>Liq Price</th>
+<th>PnL</th>
+
+</tr>
+
+</thead>
+
+<tbody>
+
+<tr>
+
+<td>BTCUSDT</td>
+<td>0.1</td>
+<td>64000</td>
+<td>64100</td>
+<td>59800</td>
+<td className="profit">+15 USDT</td>
+
+</tr>
+
+</tbody>
+
+</table>
+
+)
+
+}
