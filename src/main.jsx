8f2ff01d3261d4ee5e420ec2ef160f783c358c13
BlesUstetu <lionustetu@gmail.com>
2026-03-15 09:10:46 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,14 @@ import ReactDOM from "react-dom/client"
 import App from "./App"
 import "./styles.css"
 
+import { TradingProvider } from "./store/tradingStore"
+
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <App />
+
+<TradingProvider>
+
+<App/>
+
+</TradingProvider>
+
 )
