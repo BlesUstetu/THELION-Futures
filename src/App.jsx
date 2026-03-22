8ec9c1c0b3f6d16cddbae7c55b2d9e5764cc3f9f
BlesUstetu <lionustetu@gmail.com>
2026-03-22 16:32:04 +0900

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,13 @@
-import { TradingProvider, useTrading } from "./store/tradingStore"
 import TradingChart from "./components/TradingChart"
 import OrderPanel from "./components/OrderPanel"
 
-function Controls() {
-  const { setPair, setTimeframe } = useTrading()
-
+function App() {
   return (
-    <div style={{ position: "absolute", top: 10, left: 10 }}>
-      {["btcusdt", "ethusdt", "solusdt"].map(p => (
-        <button key={p} onClick={() => setPair(p)}>
-          {p}
-        </button>
-      ))}
-
-      {["1m", "5m", "15m", "1h"].map(tf => (
-        <button key={tf} onClick={() => setTimeframe(tf)}>
-          {tf}
-        </button>
-      ))}
+    <div>
+      <TradingChart />
+      <OrderPanel />
     </div>
   )
 }
 
-export default function App() {
-  return (
-    <TradingProvider>
-      <Controls />
-
-      <div style={{ display: "flex" }}>
-        <div style={{ flex: 3 }}>
-          <TradingChart />
-        </div>
-
-        <div style={{ flex: 1 }}>
-          <OrderPanel />
-        </div>
-      </div>
-    </TradingProvider>
-  )
-}
+export default App
