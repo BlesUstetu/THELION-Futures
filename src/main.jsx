23f33707586fb9e66b2c46efc155a1742b37051b
BlesUstetu <lionustetu@gmail.com>
2026-03-22 16:32:36 +0900

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,9 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
-
 import App from "./App"
-import "./styles.css"
-
-import { TradingProvider } from "./store/tradingStore.jsx"
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-
-  <TradingProvider>
-
-    <App/>
-
-  </TradingProvider>
-
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 )
