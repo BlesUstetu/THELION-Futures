147a806137d90abac42048dc49b03fba2c5dc280
BlesUstetu <lionustetu@gmail.com>
2026-03-22 18:32:40 +0900

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,11 @@
 import TradingChart from "./components/TradingChart"
 import OrderPanel from "./components/OrderPanel"
-import BottomTabs from "./components/BottomTabs"
 
-export default function App(){
-
-const pair="BTCUSDT"
-
-return(
-
-<div className="app">
-
-<div className="top">
-
-<div className="chart">
-<TradingChart pair={pair}/>
-</div>
-
-<OrderPanel/>
-
-</div>
-
-<BottomTabs/>
-
-</div>
-
-)
-
+export default function App() {
+  return (
+    <div className="app">
+      <TradingChart />
+      <OrderPanel />
+    </div>
+  )
 }
